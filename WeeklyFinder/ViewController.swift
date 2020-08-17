634c3e732d0596e1e6a80207f21d31872e7ab7f7
squareGTS <68609996+squareGTS@users.noreply.github.com>
2020-08-17 19:18:54 +0300

    WeeklyFinder

--- WeeklyFinder/ViewController.swift
+++ WeeklyFinder/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var dateTF: UITextField!
+    @IBOutlet weak var monthTF: UITextField!
+    @IBOutlet weak var yearTF: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dateTF.text, let month = monthTF.text, let year = yearTF.text else { return }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormater = DateFormatter()
+        dateFormater.locale = Locale(identifier: "ru_ru")
+        dateFormater.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else  { return }
+        
+        let weekday = dateFormater.string(from: date)
+        let capitalizeWeekday = weekday.capitalized
+        resultLabel.text = capitalizeWeekday
     }
-
-
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
 
